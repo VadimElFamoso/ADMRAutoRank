3964c401432ec147fda54196048984fdc048f7bc
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-05-25 16:23:29 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,497 @@
+const federations_array = [
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 3,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 4,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 5,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 6,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 7,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 8,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 9,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 10,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 11,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 12,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 1,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },   
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+
+    {
+        id: 2,
+        name: "Fédération dép. de l'Ain",
+        url: "https://www.fede01.admr.org/"
+    },
+    
+
+
+    
+]
