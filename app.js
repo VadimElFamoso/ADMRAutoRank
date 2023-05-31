65743dcbfe17c8c2e6d6afab5d7ba177f84ee3b6
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-06-01 00:09:19 +0200

    Calculating score done (chart js remaining + sort)

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
 import {data} from "./data.js";
-let api_key = "";
-let score = document.querySelectorAll(".item span")
+let api_key = "AIzaSyCfunX_X-U1WHAVR6VQ6d-mMn65V_PPu3I";
+const score_span = document.querySelectorAll(".item span")
+const status_span = document.querySelector("#title_container h2 span");
+let final_result = [];
 
-function callApi(fd_url, fd_name){
-    fetch("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" + fd_url + "&key=" + api_key + "&category=PERFORMANCE&category=ACCESSIBILITY&category=BEST_PRACTICES&category=SEO")
-        .then(response => response.json())
-        .then(json => {
+    function defineScore(index, score){
+            score_span.item(index).innerHTML = score;
+            data[index].score = score;
+            console.log(data);
+    }
+
+    //Calculate the score of each ADMR federation website :
+    async function calculateScore(){
+    
+        for(let i = 0; i < data.length; i++){
+            await fetch("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" + data[i].url + "&key=" + api_key + "&category=PERFORMANCE&category=ACCESSIBILITY&category=BEST_PRACTICES&category=SEO")
+            .then(response => response.json())
+            .then(json => {
             const lighthouse = json.lighthouseResult;
-            const categories = lighthouse.categories;
+            const categories = lighthouse.categories;     
+            let def_score = categories.performance.score * 100 + categories.accessibility.score * 100 + categories['best-practices'].score * 100 + categories.seo.score * 100; 
+            defineScore(i, def_score);
 
-                let def_score = categories.performance.score * 100 + categories.accessibility.score * 100 + categories['best-practices'].score * 100 + categories.seo.score * 100;
-                console.log(def_score);
-            //console.log(categories);
+            console.log("Score défini : item n°" + (i + 1));
+            }) 
+        }
+                        
+    } 
+    
+    function sortArray(){
+        
+    }
+    
+    //Check if each DOM federation score still contains object with empty scores return true if so, return false if not : 
+    function checkStatus(){
+        for(let i = 0; i < score_span.length; i++){
+            if(score_span[i].innerHTML = 0){
+                return false;
+            }
+            else{
+                status_span.innerHTML = "Scores attribués."
+                return true;
+            }
+        }
+    }
+
+    calculateScore();
+
+    
+
+  
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(categories);
             //console.log("Rapport" + " " + fd_name + " :");
             //console.log("Performance :" + categories.performance.score);
             //console.log("Accessibilité :" + categories.accessibility.score);
             //console.log("Bonnes pratiques :" + categories['best-practices'].score);
             //console.log("SEO :" + categories.seo.score);
-        }) 
-}
 
-    for(let i = 80; i < data.length; i++){;
-        callApi(data[i].url, data[i].name);
-        score.item.innerHTML = def_score;
-    }
 
